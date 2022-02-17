946eeda23e2e10c84671da9cf74a0e8c5e618a12
Paradorn-248 <paradorn248@gmail.com>
2022-02-17 17:39:44 +0700

    fix jwt

--- main_project.py
+++ main_project.py
@@ -10,7 +10,7 @@ from jose import jwt, JWTError
 from datetime import datetime, timedelta
 import secrets
 from typing import Optional
-
+from passlib.context import CryptContext
 
 class Registor_form(BaseModel):
     username: str
@@ -39,14 +39,24 @@ class Sensor(BaseModel):
     wind: float
 
 
-class Login(BaseModel):
-    username: str
-    password: str
-
-
 class Token(BaseModel):
     access_token: str
 
+
+class TokenData(BaseModel):
+    username: Optional[str] = None
+
+
+class User(BaseModel):
+    username: str
+    name: str
+
+
+
+class UserInDB(User):
+    hashed_password: str
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 client = MongoClient('mongodb://localhost', 27018)
 db = client["Project"]
@@ -60,6 +70,11 @@ SECRET_KEY = "d33c5f6a1d781b26efa06929e263dfe775a6c0c7bfca20dba36b4db26bbff00d"
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
+def get_password_hash(password):
+    return pwd_context.hash(password)
+
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
 
 @app.get("/")
 def start():
@@ -77,7 +92,7 @@ def reg(reg_form: Registor_form):
     else:
         init_user = {
             "username": form["username"],
-            "password": hashlib.sha256(form["password"].encode()).hexdigest(),
+            "hashed_password": get_password_hash(form["password"]),
             "name": form["name"],
             "surname": form["surname"],
             "telephone": form["telephone"]
@@ -112,7 +127,7 @@ def reg(reg_form: Registor_form):
 
 
 @app.put("/update_sensor/{username}")
-def update_sensor(sensor: Sensor,username: str):
+def update_sensor(sensor: Sensor, username: str):
     s = jsonable_encoder(sensor)
     # access_token = s["access_token"]
     # token_decoded = jwt.decode(access_token, SECRET_KEY, algorithms=['HS256'])
@@ -127,70 +142,99 @@ def update_sensor(sensor: Sensor,username: str):
     raise HTTPException(200, "Success change")
 
 
-# oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 
-def create_access_token(data: dict, secret_key: str):
+def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
     to_encode = data.copy()
-    expire = datetime.utcnow() + timedelta(minutes=30)
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=15)
     to_encode.update({"exp": expire})
-    encoded_jwt = jwt.encode(to_encode, secret_key, algorithm=ALGORITHM)
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
 
-# async def get_current_user(token: str = Depends(oauth2_scheme)):
-#     credentials_exception = HTTPException(
-#         status_code=status.HTTP_401_UNAUTHORIZED,
-#         detail="Could not validate credentials",
-#         headers={"WWW-Authenticate": "Bearer"},
-#     )
-#     try:
-#         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-#         username: str = payload.get("sub")
-#         if username is None:
-#             raise credentials_exception
-#         token_data = TokenData(username=username)
-#     except JWTError:
-#         raise credentials_exception
-#     user = get_user(fake_users_db, username=token_data.username)
-#     if user is None:
-#         raise credentials_exception
-#     return user
+
+def get_user(dbx, username: str):
+    if username in dbx:
+        user_dict = dbx[username]
+        print("check")
+        print(dbx[username])
+        return UserInDB(**user_dict)
+    
+
+def get_user_from_db() :
+    result = db_user.find({},{"_id":0})
+    #print(result)
+    dic = {}
+    for r in result:
+        dic[ r['username'] ] = r
+    return dic
+
+def get_current_user(token: str = Depends(oauth2_scheme)):
+    # print(db_user)
+    
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    ) 
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+        token_data = TokenData(username=username)
+    except JWTError:
+        raise credentials_exception
+    
+    db_userxxx = get_user_from_db()
+    
+    # for i in db_userxxx :
+    #     print(i)
+    # print(token_data.username)
+    
+    user = get_user(db_userxxx, username=token_data.username)
+    if user is None:
+        raise credentials_exception
+    return user
 
 
-# async def get_current_active_user(current_user: User = Depends(get_current_user)):
-#     if current_user.disabled:
-#         raise HTTPException(status_code=400, detail="Inactive user")
-#     return current_user
+def get_current_active_user(current_user: User = Depends(get_current_user)):
+    if current_user.disabled:
+        raise HTTPException(status_code=400, detail="Inactive user")
+    return current_user
 
-@app.post("/check")
-def check_pass(login: Login):
-    l = jsonable_encoder(login)
-    query = {"username": l["username"]}
-    res = db_user.find_one(query)
-    if res == None:
-        return {"result": "Invalid username or password"}
-    # hash_input_pass = hashlib.sha256(l["password"].encode()).hexdigest()
-    if l["password"] == res["password"]:
-        access_token = create_access_token({"sub": l["username"]}, SECRET_KEY)
-        print(access_token)
-        # raise HTTPException(202, True)
-        return {"result": True, "access_token": access_token}
-    else:
-        return {"result": False}
-        # raise HTTPException(400, False)
+def authenticate_user(fake_db, username: str, password: str):
+    user = get_user(fake_db, username)
+    if not user:
+        return False
+    if not verify_password(password, user.hashed_password):
+        return False
+    return user
+
+
+
+@app.post("/token", response_model=Token)
+async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends()):
+    users_db = get_user_from_db()
+    user = authenticate_user(users_db, form_data.username, form_data.password)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={"sub": user.username}, expires_delta=access_token_expires
+    )
+    return {"access_token": access_token, "token_type": "bearer"}
 
 
 @app.get("/get_sensor")
-def get_sensor(token: Token):
-    t = jsonable_encoder(token)
-    access_token = t["access_token"]
-    try:
-        # ถ้าdecodeไม่ผ่าน จะเกิดinternal server errorทันที ทำให้ทำคำสั่งต่อไปไม่ได้
-        token_decoded = jwt.decode(
-            access_token, SECRET_KEY, algorithms=['HS256'])
-        print(token_decoded)
-        query = {"username": token_decoded["sub"]}
-        res = db_home.find_one(query, {"_id": 0})
-        return {"result": res}
-    except:
-        return {"result": "Can't decode"}
+def get_sensor(current_user: User = Depends(get_current_user)):
+    query = {"username": current_user.username}
+    res = db_home.find_one(query, {"_id": 0})
+    return {"result": res}
