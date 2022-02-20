eec12f7e8048ed52a79dea3c337e013bda6dd96a
Paradorn-248 <paradorn248@gmail.com>
2022-02-20 16:42:51 +0700

    latest

---
+++ test_auth.py
@@ -0,0 +1,183 @@
+
+   
+# https://fastapi.tiangolo.com/tutorial/security/simple-oauth2/
+from datetime import datetime, timedelta
+from typing import Optional
+
+from pymongo import MongoClient
+
+from fastapi import Depends, FastAPI, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from jose import JWTError, jwt
+from passlib.context import CryptContext
+from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
+
+# to get a string like this run:
+# openssl rand -hex 32
+SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+app = FastAPI()
+
+fake_users_db = {
+    "johndoe": {
+        "username": "johndoe",
+        "cafe_name": "John Doe",
+        "hashed_password": "$2b$12$EixZaYVK1fsbw1ZfbX3OXePaWxn96p36WQoeG6Lruj3vjPGga31lW"
+    },
+    "stamp01": {
+        "username": "stamp01",
+        "cafe_name": "Stamp Cafe",
+        "hashed_password": "$2b$12$gojzLZ51JuVgweLMgCVjSeUSc9d/MNmXhIoikzbizBYLW1zni1632"
+    },
+    "stamp04": {
+        "username": "stamp04",
+        "cafe_name": "Stamp 444 Cafe",
+        "hashed_password": "$2b$12$huDWlTMxZbq4bcDkyVuJYO9udObObHiw1H49A3XJJHv6vIApSLKDC"
+    },
+}
+
+
+myclient = MongoClient('mongodb://localhost', 27018)
+db = myclient["Project"]
+collection = db["User"]
+
+origins = ["*"]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+class TokenData(BaseModel):
+    username: Optional[str] = None
+
+
+class User(BaseModel):
+    username: str
+    name: str
+    surname: str
+    telephone: str
+
+
+class UserInDB(User):
+    hashed_password: str
+
+
+
+def get_user_from_db() :
+    result = collection.find({},{"_id":0})
+    #print(result)
+    dic = {}
+    for r in result:
+        dic[ r['username'] ] = r
+    return dic
+
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
+
+def get_password_hash(password):
+    return pwd_context.hash(password)
+
+
+def get_user(db, username: str):
+    if username in db:
+        user_dict = db[username]
+        return UserInDB(**user_dict)
+
+
+def authenticate_user(fake_db, username: str, password: str):
+    user = get_user(fake_db, username)
+    if not user:
+        return False
+    if not verify_password(password, user.hashed_password):
+        return False
+    return user
+
+
+def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+
+async def get_current_user(token: str = Depends(oauth2_scheme)):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        #print(payload)
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+        token_data = TokenData(username=username)
+    except JWTError:
+        raise credentials_exception
+    users_db = get_user_from_db()
+    user = get_user(users_db, username=token_data.username)
+    if user is None:
+        raise credentials_exception
+    return user
+
+
+async def get_current_active_user(current_user: User = Depends(get_current_user)):
+    #if current_user.disabled:
+    #    raise HTTPException(status_code=400, detail="Inactive user")
+    return current_user
+
+
+@app.post("/token", response_model=Token)
+async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends()):
+    users_db = get_user_from_db()
+    '''print(users_db)
+    print(type(users_db))
+    print("---------")
+    print(fake_users_db)
+    print(type(fake_users_db))'''
+    user = authenticate_user(users_db, form_data.username, form_data.password)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={"sub": user.username}, expires_delta=access_token_expires
+    )
+    return {"access_token": access_token, "token_type": "bearer"}
+
+@app.get("/")
+async def read_users_me():
+    return "HELLOOOOOOOOOOOOOOOOO"
+
+@app.get("/users/me/", response_model=User)
+async def read_users_me(current_user: User = Depends(get_current_active_user)):
+    return current_user
+
+@app.get("/users/me/items/")
+async def read_own_items(current_user: User = Depends(get_current_user)):
+    return [{"item_id": "Foo", "owner": current_user.username}]
